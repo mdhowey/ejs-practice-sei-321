acbe1d0aa9cbfa52f3b0037066cbda30387025b7
devhowey <dev.howey@gmail.com>
2022-04-18 21:17:16 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+  res.render('index')
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running properly`);
+  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on Port: ${PORT}`);
+});
